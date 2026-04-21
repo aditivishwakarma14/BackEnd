c8054e83c07f7f7f87dfeee5c4b16d51b4138689
aditivishwakarma14 <aditivishwakarmacoding@gmail.com>
2026-04-21 18:46:27 +0530

    Frontend integration complete

---
+++ day-11/server.js
@@ -0,0 +1,11 @@
+
+const app = require('./src/app')
+const connectToDb = require('./src/config/database')
+require("dotenv").config()
+
+
+connectToDb()
+
+app.listen(3000 , ()=>{
+    console.log("Server is running on port no. 3000")
+})
---
+++ day-11/src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+function connectToDb(){
+    mongoose.connect(process.env.MODLE_URI)
+    .then(()=>{
+        console.log("Connected to database")
+    })
+}
+
+module.exports = connectToDb
